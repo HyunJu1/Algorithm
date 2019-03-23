d4d90693dd4df8b9e80f2a72a0301eb82d7354bb
Hyunju1 <hh960305@naver.com>
2019-03-24 02:10:51 +0900

    swea 1220 magnetic

--- SW_Level3/src/Day2/Q1220_Magnetic.java
+++ SW_Level3/src/Day2/Q1220_Magnetic.java
@@ -1,13 +1,45 @@
 package Day2;
 
+import java.util.Scanner;
+
 public class Q1220_Magnetic {
 
 	public static void main(String[] args) {
-	
-		
-		
-		
-		
+
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		int arr[][] = new int[num][num];
+
+		for (int a = 1; a <= 10; a++) {
+			int cnt = 0;
+			for (int i = 0; i < num; i++) {
+				for (int j = 0; j < num; j++) {
+					arr[i][j] = sc.nextInt();
+
+				}
+			}
+
+			for (int i = 0; i < num; i++) {
+				int check = 0;
+				for (int j = 0; j < num; j++) {
+
+					if (arr[j][i] == 0) {
+						continue;
+					} else if (check == 0 && arr[j][i] == 2) {
+						continue;
+					} else if (check == 1 && arr[j][i] == 2) {
+						cnt++;
+						check = 0;
+					} else if (arr[j][i] == 1) {
+						check = 1;
+					}
+
+				}
+				
+			}
+			System.out.println("#" + (a) + " " + cnt);
+		}
 	}
 
 }
---
+++ SW_Level3/src/Day3/Q5515_2016요일.java
@@ -0,0 +1,30 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class Q5515_2016ø‰¿œ {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+
+		
+		
+		
+		
+		for (int i = 0; i < num; i++) {
+			
+			int result= 0; 
+			
+			
+			
+			
+			
+			System.out.println("#" + (i + 1) + " " + result);
+
+		}		
+		
+	}
+}
